c10160e9a8f869b543cd66d46448cb2d2f5021c9
jagottsicher <jagottsicher@gmx.net>
2024-01-19 13:15:54 +0800

    added lecture 14

--- blockchain.go
+++ blockchain.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+type Block struct {
+	nonce        int
+	previousHash string
+	timestamp    int64
+	transactions []string
+}
+
+func NewBlock(nonce int, previousHash string) *Block {
+	b := new(Block)
+	b.timestamp = time.Now().UnixNano()
+	b.nonce = nonce
+	b.previousHash = previousHash
+	return b
+}
+
+func (b *Block) Print() {
+	fmt.Printf("timestamp\t%d\n", b.timestamp)
+	fmt.Printf("nonce\t\t%d\n", b.nonce)
+	fmt.Printf("previous_hash\t%s\n", b.previousHash)
+	fmt.Printf("transactions\t%s\n", b.transactions)
+}
+
+func init() {
+	log.SetPrefix("Blockchain Node: ")
+}
 
 func main() {
-	fmt.Println("Hello world")
+	b := NewBlock(0, "init hash")
+	b.Print()
 }
