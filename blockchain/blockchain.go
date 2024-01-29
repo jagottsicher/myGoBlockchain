b414068d3c7ff88ec00a221c5ca4fe1fbc599dfc
jagottsicher <jagottsicher@gmx.net>
2024-01-29 10:40:00 +0800

    added a wallet and created private public key pairs

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,12 +1,9 @@
-package main
+package blockchain
 
 import (
-	"crypto/sha256"
-	"encoding/json"
 	"fmt"
 	"log"
 	"strings"
-	"time"
 )
 
 const (
@@ -14,50 +11,6 @@ const (
 	MINING_SENDER     = "BLOCKCHAIN REWARD SYSTEM (e.g. minting & fees)"
 	MINING_REWARD     = 1.0
 )
-
-type Block struct {
-	timestamp    int64
-	nonce        int
-	previousHash [32]byte
-	transactions []*Transaction
-}
-
-func NewBlock(nonce int, previousHash [32]byte, transactions []*Transaction) *Block {
-	b := new(Block)
-	b.timestamp = time.Now().UnixNano()
-	b.nonce = nonce
-	b.previousHash = previousHash
-	b.transactions = transactions
-	return b
-}
-
-func (b *Block) Print() {
-	fmt.Printf("timestamp\t%d\n", b.timestamp)
-	fmt.Printf("nonce\t\t%d\n", b.nonce)
-	fmt.Printf("previous_hash\t%x\n", b.previousHash)
-	for _, t := range b.transactions {
-		t.Print()
-	}
-}
-
-func (b *Block) Hash() [32]byte {
-	m, _ := json.Marshal(b)
-	return sha256.Sum256([]byte(m))
-}
-
-func (b *Block) MarshalJSON() ([]byte, error) {
-	return json.Marshal(struct {
-		Timestamp    int64          `json:"timestamp"`
-		Nonce        int            `json:"nonce"`
-		PreviousHash [32]byte       `json:"previous_hash"`
-		Transactions []*Transaction `json:"transactions"`
-	}{
-		Timestamp:    b.timestamp,
-		Nonce:        b.nonce,
-		PreviousHash: b.previousHash,
-		Transactions: b.transactions,
-	})
-}
 
 type Blockchain struct {
 	transactionPool   []*Transaction
@@ -150,56 +103,4 @@ func (bc *Blockchain) CalculateTotalAmount(blockchainAddress string) float32 {
 		}
 	}
 	return totalAmount
-}
-
-type Transaction struct {
-	senderBlockchainAddress    string
-	recipientBlockchainAddress string
-	value                      float32
-}
-
-func NewTransaction(sender string, recipient string, value float32) *Transaction {
-	return &Transaction{sender, recipient, value}
-}
-
-func (t *Transaction) Print() {
-	fmt.Printf("%s\n", strings.Repeat("-", 40))
-	fmt.Printf(" sender_blockchain_address\t%s\n", t.senderBlockchainAddress)
-	fmt.Printf(" recipient_blockchain_address\t%s\n", t.recipientBlockchainAddress)
-	fmt.Printf(" value\t\t\t\t%1f\n", t.value)
-}
-
-func (t *Transaction) MarshalJSON() ([]byte, error) {
-	return json.Marshal(struct {
-		Sender    string  `json:"sender_blockchain_address"`
-		Recipient string  `json:"recipient_blockchain_address"`
-		Value     float32 `json:"value"`
-	}{
-		Sender:    t.senderBlockchainAddress,
-		Recipient: t.recipientBlockchainAddress,
-		Value:     t.value,
-	})
-}
-
-func init() {
-	log.SetPrefix("Blockchain Node: ")
-}
-
-func main() {
-	minerBlockchainAddress := "miners_blockchain_address"
-	blockChain := NewBlockchain(minerBlockchainAddress)
-	blockChain.Print()
-
-	blockChain.AddTransaction("Rick", "Morty", 137)
-	blockChain.Mining()
-	blockChain.Print()
-
-	blockChain.AddTransaction("Stan", "Francine", 23)
-	blockChain.AddTransaction("Fry", "Bender", 42)
-	blockChain.Mining()
-	blockChain.Print()
-
-	fmt.Printf("Miner %.1f\n", blockChain.CalculateTotalAmount(minerBlockchainAddress))
-	fmt.Printf("Stan %.1f\n", blockChain.CalculateTotalAmount("Stan"))
-	fmt.Printf("Francine %.1f\n", blockChain.CalculateTotalAmount("Francine"))
 }
