9d0f8bd44d689758c95580f593f738967f0f35c8
jagottsicher <jagottsicher@gmx.net>
2024-02-02 09:23:36 +0800

    added a first route and get request handler to the blockchain node

--- blockchain_node/blockchain_node.go
+++ blockchain_node/blockchain_node.go
@@ -5,7 +5,12 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/jagottsicher/myGoBlockchain/blockchain"
+	"github.com/jagottsicher/myGoBlockchain/wallet"
 )
+
+var cache map[string]*blockchain.Blockchain = make(map[string]*blockchain.Blockchain)
 
 type BlockchainNode struct {
 	port uint16
@@ -19,12 +24,35 @@ func (bcn *BlockchainNode) Port() uint16 {
 	return bcn.port
 }
 
-func HelloWorld(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World")
+func (bcn *BlockchainNode) GetBlockchain() *blockchain.Blockchain {
+	bc, ok := cache["blockchain"]
+	if !ok {
+		minerWallet := wallet.NewWallet()
+		bc = blockchain.NewBlockchain(minerWallet.BlockchainAddress(), bcn.Port())
+		cache["blockchain"] = bc
+		log.Printf("Public Key %v", minerWallet.PublicKeyStr())
+		log.Printf("Private Key %v", minerWallet.PrivateKeyStr())
+		log.Printf("Blockchain Address %v", minerWallet.BlockchainAddress())
+	}
+	return bc
+}
+
+func (bcn *BlockchainNode) GetChain(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+
+	case http.MethodGet:
+		w.Header().Add("Content-Type", "application/json")
+		bc := bcn.GetBlockchain()
+		m, _ := bc.MarshalJSON()
+		io.WriteString(w, string(m[:]))
+	default:
+		log.Printf("Error: Invalid HTTP method")
+	}
+
 }
 
 func (bcn *BlockchainNode) Run() {
-	http.HandleFunc("/", HelloWorld)
+	http.HandleFunc("/", bcn.GetChain)
 
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(bcn.Port())), nil))
 }
