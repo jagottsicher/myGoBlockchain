ad89a604f04244e1d2d0c7c165937e75acd8be0f
jagottsicher <jagottsicher@gmx.net>
2024-02-10 20:13:52 +0800

    added automated mining trigger

--- blockchain_node/blockchain_node.go
+++ blockchain_node/blockchain_node.go
@@ -126,10 +126,26 @@ func (bcn *BlockchainNode) Mine(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (bcn *BlockchainNode) StartMine(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		bc := bcn.GetBlockchain()
+		bc.StartMining()
+
+		m := utils.JsonStatus("success")
+		w.Header().Add("Content-Type", "application/json")
+		io.WriteString(w, string(m))
+	default:
+		log.Println("ERROR: Invalid HTTP method")
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
 func (bcn *BlockchainNode) Run() {
 	http.HandleFunc("/", bcn.GetChain)
 	http.HandleFunc("/transactions", bcn.Transactions)
 	http.HandleFunc("/mine", bcn.Mine)
+	http.HandleFunc("/mine/start", bcn.StartMine)
 
 	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(bcn.Port())), nil))
 }
