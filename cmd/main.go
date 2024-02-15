e9a83caa04577739d66435b75b23ec80b1788078
jagottsicher <jagottsicher@gmx.net>
2024-02-15 15:48:00 +0800

    enhancement of chanhign from local IP 127.0.0.1 to external reachable ip

--- cmd/main.go
+++ cmd/main.go
@@ -10,5 +10,10 @@ func main() {
 	// fmt.Println(utils.IsFoundNode("127.0.0.1", 3333))
 	// fmt.Println(utils.IsFoundNode("localhost", 3333))
 
-	fmt.Println(utils.FindNeighbors("127.0.0.1", 3333, 0, 3, 3333, 3336))
+	// fmt.Println(utils.FindNeighbors("127.0.0.1", 3333, 0, 3, 3333, 3336))
+
+	// fmt.Println(utils.GetHost())
+
+	myAddress := utils.GetHost()
+	fmt.Println(utils.FindNeighbors(myAddress, 3333, 0, 3, 3333, 3336))
 }
